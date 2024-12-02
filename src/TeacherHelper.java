456be28016d9ccf4b0daf80f29fac00aea5703e1
na1santos <dingao.voador@gmail.com>
2024-12-02 19:54:19 -0300

    Bug fixes

--- src/TeacherHelper.java
+++ src/TeacherHelper.java
@@ -1,11 +1,12 @@
 import java.util.Random;
+import java.util.Scanner;
 
 public class TeacherHelper {
 
-    public static final double horaAula = 17.50;
+//    public static final double horaAula = 17.50;
     public static void main(String[] args) {
         int opcao=0, numeroAulas, qtdeAlunos, i;
-        double salarioBase, horaAtividade, descansoSemanalRemunerado, salario, notaAluno, mediaAlunos;
+        double salarioBase, horaAtividade, descansoSemanalRemunerado, salario, notaAluno, mediaAlunos, horaAula;
 
         Random gerador = new Random();
 
@@ -17,6 +18,7 @@ public class TeacherHelper {
             System.out.println("1 - Calcular salário");
             System.out.println("2 - Calcular média de notas dos alunos");
             System.out.println("3 - Exibir a motivação do dia!");
+            System.out.println("4 - Encerrar o sistema");
             opcao = leitor.nextInt();
 
             switch (opcao) {
@@ -26,6 +28,8 @@ public class TeacherHelper {
                      * http://www1.sinprosp.org.br/guia_consultas.asp?mat=8*/
                     System.out.println("Para calcular seu salário base precisamos saber quantas aulas semanais o professor tem na instituição");
                     numeroAulas = leitor.nextInt();
+                    System.out.println("Informe o valor da hora-aula praticado pela instituição: ");
+                    horaAula = leitor.nextDouble();
                     salarioBase = numeroAulas * 4.5 * horaAula;
                     horaAtividade = salarioBase * 0.05;
                     descansoSemanalRemunerado = (salarioBase + horaAtividade) / 6;
@@ -47,6 +51,7 @@ public class TeacherHelper {
                         System.out.println("Digite a nota do " + (i+1) + "º aluno:");
                         notaAluno = leitor.nextDouble();
                         mediaAlunos = mediaAlunos + notaAluno;
+                        i++;
                     }
                     mediaAlunos = mediaAlunos / qtdeAlunos;
                     System.out.println("A média de notas dos alunos dessa turma é " + mediaAlunos);
